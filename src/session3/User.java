194b59c8b8d1a7810169d5841a2bafbbbc81ec65
NguyenQuynhThu <letter.japan@gmail.com>
2019-04-23 00:00:47 +0700

    testgit

--- src/session3/User.java
+++ src/session3/User.java
@@ -1,25 +1,46 @@
 package session3;
 
-public class User {
-    public int id;
-    public String name;
-    public String email;
-    public String password;
+import java.util.Scanner;
 
-    public User(){
+public class TamGiac {
+    public int a;
+    public int b;
+    public int c;
+
+
+    public TamGiac(){
     }
-    //hàm khởi tạo: Tên giống class, tự động chạy không cần thực thi, có thể có tham số hoặc không
 
-    public User(int id, String name, String email, String password){
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.password = password;
+    public TamGiac(int a, int b, int c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public void nhap3Canh() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhap canh a: ");
+        a = scanner.nextInt();
+        System.out.print("Nhap canh b: ");
+        b = scanner.nextInt();
+        System.out.print("Nhap canh c: ");
+        c = scanner.nextInt();
+
+    }
+
+
+
+
+
+    public boolean kiemTraBaCanh(){
+        if ((a + b > c) || (a + c > b) || (b + c > a)) {
+            System.out.println("Đây là tam giác !");
+            return true;
+        }
     }
 
     public static void main(String args[]){
-        User u1 = new User();
-        User u2 = new User(1, "Le Van A" , "a@gmai.com", "123456");
+        TamGiac tg = new TamGiac();
 
     }
 }
